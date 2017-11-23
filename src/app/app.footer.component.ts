1927565b105ee8a32c49045033d7acff338dfc49
cagatay.civici <cagatay.civici@fd9223c1-c77d-41e1-af93-17165896985c>
2017-11-23 22:11:55 +0000

    fixes on theme

--- src/app/app.footer.component.ts
+++ src/app/app.footer.component.ts
@@ -6,7 +6,7 @@ import {Component} from '@angular/core';
         <div class="footer">
             <div class="card clearfix">
                 <span class="footer-text-left">PrimeNG Barcelona for Angular</span>
-                <span class="footer-text-right"><span class="ui-icon ui-icon-copyright"></span>  <span>All Rights Reserved</span></span>
+                <span class="footer-text-right"><span class="material-icons ui-icon-copyright"></span>  <span>All Rights Reserved</span></span>
             </div>
         </div>
     `
